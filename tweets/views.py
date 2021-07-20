5109ba43958f378156d39d69b37dff91b48c3819
Th3At0nic <islammdrahatul@gmail.com>
2021-07-20 11:21:19 +0600

    added home.html

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
-
+from .models import Tweet
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    """
+    REST API VIEW
+    Consume by JavaScript/ Swift/ java/ iOS/Android
+    return json data
+    """
+
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    # json.dumps content_type="application/json"
+    return JsonResponse(data, status=status)
